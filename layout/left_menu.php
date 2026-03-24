bdf2ebfb0eb2a76dee4569ab569701c051c7691f
Nagat0-0 <yatsko.ihor@chnu.edu.ua>
2026-03-24 13:32:15 +0200

    Lab_3

--- layout/left_menu.php
+++ layout/left_menu.php
@@ -3,6 +3,7 @@
                 <ul>
                     <li><a href="index.php?action=main">Головна</a></li>
                     <li><a href="index.php?action=about">Про сайт</a></li>
+                    <li><a href="index.php?action=registration">Реєстрація</a></li>
                 </ul>
             </nav>
 </aside>
---
+++ views/registration.php
@@ -0,0 +1,82 @@
+<?php
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $login = $_POST['login'] ?? '';
+    $password = $_POST['password'] ?? '';
+    $password_repeat = $_POST['password_repeat'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $first_name = $_POST['first_name'] ?? '';
+
+    if (!preg_match('/^(?=(?:.*[a-zA-Zа-яА-ЯіІїЇєЄґҐ]){4})[a-zA-Zа-яА-ЯіІїЇєЄґҐ0-9_-]+$/u', $login)) {
+        $errors[] = "Логін має містити не менше 4 літер. Дозволено використовувати латинські/кириличні букви, цифри, підкреслення та дефіс.";
+    }
+
+    if (!preg_match('/^(?=(?:.*[a-zA-Zа-яА-ЯіІїЇєЄґҐ]){7})(?=.*[a-zа-яієїґ])(?=.*[A-ZА-ЯІЄЇҐ])(?=.*\d).+$/u', $password)) {
+        $errors[] = "Пароль має містити не менше 7 літер, серед яких обов'язково є великі та малі, а також мінімум одну цифру.";
+    }
+
+    if ($password !== $password_repeat) {
+        $errors[] = "Введені паролі не співпадають.";
+    }
+
+    if (!preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', $email)) {
+        $errors[] = "Введіть коректну Email-адресу.";
+    }
+
+    if (!preg_match("/^[a-zA-Zа-яА-ЯіІїЇєЄґҐ'\-\s]{0,255}$/u", $first_name)) {
+        $errors[] = "Поле 'Ім'я' не має перевищувати 255 символів і не може містити цифр чи розділових знаків (окрім дефісу та апострофа).";
+    }
+
+    if (empty($errors)) {
+        header("Location: index.php?action=registration_successful");
+        exit;
+    }
+}
+?>
+
+<main class="content">
+    <h2>Реєстрація нового користувача</h2>
+
+    <p style="font-size: 0.9em; color: #555;">Поля, позначені <span style="color: red;">*</span>, є обов'язковими для заповнення.</p>
+
+    <?php if (!empty($errors)): ?>
+        <div class="error-messages">
+            <strong>Помилки при заповненні форми:</strong>
+            <ul>
+                <?php foreach ($errors as $error): ?>
+                    <li><?php echo $error; ?></li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    <?php endif; ?>
+
+    <form action="index.php?action=registration" method="POST">
+        <div class="form-group">
+            <label for="login">Логін <span style="color: red;">*</span>:</label><br>
+            <input type="text" id="login" name="login" required>
+        </div>
+
+        <div class="form-group">
+            <label for="password">Пароль <span style="color: red;">*</span>:</label><br>
+            <input type="password" id="password" name="password" required>
+        </div>
+
+        <div class="form-group">
+            <label for="password_repeat">Повторіть пароль <span style="color: red;">*</span>:</label><br>
+            <input type="password" id="password_repeat" name="password_repeat" required>
+        </div>
+
+        <div class="form-group">
+            <label for="email">Електронна пошта <span style="color: red;">*</span>:</label><br>
+            <input type="text" id="email" name="email" required>
+        </div>
+
+        <div class="form-group">
+            <label for="first_name">Ім'я :</label><br>
+            <input type="text" id="first_name" name="first_name">
+        </div>
+
+        <button type="submit" class="btn-submit">Зареєструватися</button>
+    </form>
+</main>
